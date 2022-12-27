77fb28b47d0498b227035c1686899025ac37618b
wolverine79937 <wolverine79936@gmail.com>
2022-12-26 23:05:04 -0700

    I know. This is trivial, but I felt that it was important. Adding a version.hh file.

---
+++ version.hh
@@ -0,0 +1,10 @@
+/* This is my version history file.
+*
+*  0.0.0 (12/25/2022): This was the initial pre-prototype of my game. It was nothing but a circle that the player could
+*                      move around the screen with the A, W, S and D keys.
+*
+*  0.0.1 (12/26/2022): Finally got rid of that ugly green circle and replaced it with a neat jet sprite. I really couldn't
+*                      tollerate that green circle much longer. LOL.
+*/
+
+cVersion = "0.0.1";
